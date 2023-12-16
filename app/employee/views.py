e687acca2f272d3ecdc3bfb912d0e04b80158edb
Kavin Palanisamy <pkavin04@gmail.com>
2023-12-16 12:43:08 +0530

    model for db added

--- app/employee/views.py
+++ app/employee/views.py
@@ -1,22 +1,14 @@
 from flask import Blueprint, jsonify,request
-from app.models import get_db,User
+from app.models import get_db,Employee
 from app.employee import employee_bp
 
-from datetime import datetime
 
-
-#sample route to use the database
-#add all employee routes here
-@employee_bp.route('/')
+@employee_bp.route('/',methods=['POST'])
 def employee_dashboard():
-    # print("Hi")
     data = request.get_json()
     username = data.get('username')
-    email = data.get('email')
-
-    new_user = User(username=username, email=email)
-
-    # Save the User object to the 'users' collection
+    password = data.get('password')
+    adminid = data.get('adminid')
+    new_user = Employee(username=username,password=password,adminid=adminid)
     new_user.save()
-
     return jsonify({"message": "User created successfully"}), 201
