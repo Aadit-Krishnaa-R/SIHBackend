f4dfa7400170e8056f470b17a321c36295aa6d7f
Saranya <mssaranyashree@gmail.com>
2023-12-16 14:35:17 +0530

    login/signup fix

--- app/employee/views.py
+++ app/employee/views.py
@@ -1,22 +1,87 @@
-from flask import Blueprint, jsonify,request
+from flask import Blueprint, request, jsonify, session, redirect, url_for
+from flask_pymongo import PyMongo
 from app.models import get_db,User
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.employee import employee_bp
+from run import app
+mongo = PyMongo(app)
 
 from datetime import datetime
 
 
 #sample route to use the database
 #add all employee routes here
-@employee_bp.route('/')
-def employee_dashboard():
-    # print("Hi")
+#@employee_bp.route('/')
+#def employee_dashboard():
+    
+   
+
+@employee_bp.route('/employee_signup', methods=['POST'])
+def emp_signup():
     data = request.get_json()
+
     username = data.get('username')
-    email = data.get('email')
+    password = data.get('password')
 
-    new_user = User(username=username, email=email)
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required'}), 400
 
-    # Save the User object to the 'users' collection
-    new_user.save()
+    if mongo.db.employees.find_one({'username': username}):
+        return jsonify({'error': 'Username already taken'}), 400
 
-    return jsonify({"message": "User created successfully"}), 201
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+    new_user = {'username': username, 'password': hashed_password}
+    mongo.db.employees.insert_one(new_user)
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+
+
+
+
+
+@employee_bp.route('/employee_login', methods=['POST'])
+def emp_login():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required'}), 400
+
+    employee = mongo.db.employees.find_one({'username': username})
+
+    if employee and check_password_hash(employee['password'], password):
+        
+        session['employee'] = {'username': username}
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+
+
+
+@employee_bp.route('/employee_logout')
+def emp_logout():
+    
+    session.pop('employee', None)
+    return jsonify({'message': 'Logout successful'}), 200
+
+
+
+@employee_bp.route('/employee_profile')
+def emp_profile():
+    
+    if 'employee' not in session:
+        return jsonify({'error': 'Not logged in'}), 401
+
+    
+    user_info = session['employee']
+    return jsonify({'username': user_info['username']}), 200
+
+
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,170 +1,125 @@
 from pymongo import MongoClient
 from datetime import datetime
 from app.config import MONGO_URI, DB_NAME
+from bson import ObjectId
 
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
-#Sample Model, Create all models like this just copy paste and edit
-class User:
-    def __init__(self, username, email, created_at=None):
+class Employee:
+    def __init__(self,username,password,adminid,created_at=None):
         self.username = username
-        self.email = email
+        self.password = password
+        self.adminid = adminid
         self.created_at = created_at or datetime.utcnow()
 
     def to_dict(self):
         return {
             'username': self.username,
-            'email': self.email,
+            'password': self.password,
+            'adminid': self.adminid,
             'created_at': self.created_at
         }
 
     def save(self):
-        # Access the 'users' collection
-        users_collection = db['users']
+        employee_collection = db['employee']
+        employee_data = self.to_dict()
+        employee_collection.insert_one(employee_data)
 
-        # Convert the User object to a dictionary
-        user_data = self.to_dict()
+    def get_employee_by_id(employee_id):
+        employee_collection = db['employee']
+        return employee_collection.find_one({'_id': ObjectId(employee_id)})
 
-        # Insert the user document into the 'users' collection
-        users_collection.insert_one(user_data)
+    def update(self, employee_id):
+        employee_collection = db['employee']
+        employee_collection.update_one({'_id': ObjectId(employee_id)}, {'$set': self.to_dict()})
 
-def get_db():
-    return db
+    def delete(self, employee_id):
+        employee_collection = db['employee']
+        employee_collection.delete_one({'_id': ObjectId(employee_id)})
+
+class Call:
+    def __init__(self,graph_coords,emotions,pos_percent,neg_percent,rating,language,duration,gender,employeid,created_at=None):
+        self.graph_coords = graph_coords
+        self.emotions = emotions
+        self.pos_percent =pos_percent
+        self.neg_percent =neg_percent
+        self.rating = rating
+        self.language = language
+        self.duration = duration
+        self.gender = gender
+        self.employeid = employeid
+
+        self.created_at = created_at or datetime.utcnow()
+
+        def to_dict(self):
+            return {
+            'graph_coords' : self.graph_coords,
+            'emotions' : self.emotions,
+            'pos_percent' : self.pos_percent, 
+            'neg_percent' : self.neg_percent,
+            'rating' : self.rating,
+            'language' : self.language,
+            'duration' : self.duration,
+            'gender' : self.gender,
+            'employeid' : self.employeid,
+            'created_at': self.created_at
+        }
+
+    def save(self):
+        call_collection = db['calls']
+        call_data = self.to_dict()
+        call_collection.insert_one(call_data)
+
+    def get_call_by_id(call_id):
+        call_collection = db['calls']
+        return call_collection.find_one({'_id': ObjectId(call_id)})
+
+    def update(self, call_id):
+        call_collection = db['calls']
+        call_collection.update_one({'_id': ObjectId(call_id)}, {'$set': self.to_dict()})
+
+    def delete(self, call_id):
+        call_collection = db['calls']
+        call_collection.delete_one({'_id': ObjectId(call_id)})
+
+    class Admin:
+        def __init__(self,username,password,created_at=None):
+            self.username = username
+            self.password = password
+            self.created_at = created_at or datetime.utcnow()
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'password': self.password,
+            'created_at': self.created_at
+        }
+
+    def save(self):
+        admin_collection = db['admin']
+        admin_data = self.to_dict()
+        admin_collection.insert_one(admin_data)
+    def get_admin_by_id(admin_id):
+        admin_collection = db['admin']
+        return admin_collection.find_one({'_id': ObjectId(admin_id)})
+
+    def update(self, admin_id):
+        admin_collection = db['admin']
+        admin_collection.update_one({'_id': ObjectId(admin_id)}, {'$set': self.to_dict()})
+
+    def delete(self, admin_id):
+        admin_collection = db['admin']
+        admin_collection.delete_one({'_id': ObjectId(admin_id)})
 
 
-
-
-
-
-
-# # # # app/models.py
-
-# # from datetime import datetime
-# # # from app import mongo
-# # # from flask import current_app
-
-
-
-# # import bson
-
-# # from flask import current_app, g
-# # from werkzeug.local import LocalProxy
-# # from flask_pymongo import PyMongo
-# # from pymongo.errors import DuplicateKeyError, OperationFailure
-# # from bson.objectid import ObjectId
-# # from bson.errors import InvalidId
-
-
-# # def get_db():
-# #     """
-# #     Configuration method to return db instance
-# #     """
-# #     db = getattr(g, "_database", None)
-
-# #     if db is None:
-
-# #         db = g._database = PyMongo(current_app).db
-       
-# #     return db
-
-
-# # # Use LocalProxy to read the global db instance with just `db`
-# # db = LocalProxy(get_db)
-
-# # print(type(db))
-
-# # # class User:
-# # #     def __init__(self, username, email):
-# # #         self.username = username
-# # #         self.email = email
-# # #         self.created_at = datetime.utcnow()
-
-
-    
-# # #     def save(self):
-# # #         # Insert the user document into the "users" collection
-# # #         # mongo = current_app.extensions['mongo']
-# # #         # mongo = current_app.extensions.get('pymongo')
-# # #         db.users.insert_one({
-# # #             'username': self.username,
-# # #             'email': self.email,
-# # #             'created_at': self.created_at
-# # #         })
-# # #         print(type(db.users))
-
-# # #     @staticmethod
-# # #     def get_all():
-# # #         # mongo = current_app.extensions['mongo']
-# # #         # mongo = current_app.extensions.get('pymongo')
-# # #         # Retrieve all users from the "users" collection
-# # #         return list(db.users.find())
-
-
-
-# from datetime import datetime
-# from flask import current_app, g
-# from werkzeug.local import LocalProxy
-# from flask_pymongo import PyMongo
-# from pymongo.errors import DuplicateKeyError
-# from bson.objectid import ObjectId
-
-# def get_db():
-#     db = getattr(g, "_database", None)
-
-#     if db is None:
-#         db = g._database = PyMongo(current_app).db
-
-#     return db
-
-# db = LocalProxy(get_db)
-
-# class User:
-#     def __init__(self, username, email):
-#         self.username = username
-#         self.email = email
-#         self.created_at = datetime.utcnow()
-
-#     def save(self):
-#         try:
-#             db.users.insert_one({
-#                 'username': self.username,
-#                 'email': self.email,
-#                 'created_at': self.created_at
-#             })
-#         except DuplicateKeyError:
-#             # Handle duplicate key error if needed
-#             pass
-
-#     @staticmethod
-#     def get_all():
-#         return list(db.users.find())
+    def get_db():
+        return db
 
 
 
-# models.py
 
 
 
-# def create_user(username, email):
-#     user = {
-#         'username': username,
-#         'email': email,
-#         'created_at': datetime.utcnow()
-#     }
 
-#     users_collection.insert_one(user)
-
-# def get_users():
-#     users = users_collection.find()
-#     users_data = [
-#         {
-#             'username': user['username'],
-#             'email': user['email'],
-#             'created_at': user['created_at'].strftime('%Y-%m-%d %H:%M:%S')
-#         }
-#         for user in users
-#     ]
-
-#     return users_data
